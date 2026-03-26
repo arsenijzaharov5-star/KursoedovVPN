786d9322fe77f1deb5c5f40c1cfffd4c4e9e6fe3
root <root@1415788.xorek.cloud>
2026-03-26 03:22:49 +0300

    fix(build): import DropShadowEffect namespace for main button animation

--- v2rayN/v2rayN/Views/MainWindow.xaml.cs
+++ v2rayN/v2rayN/Views/MainWindow.xaml.cs
@@ -2,6 +2,7 @@ using System.IO;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
 using System.Windows.Threading;
 using MaterialDesignThemes.Wpf;
 using ServiceLib.Handler;
